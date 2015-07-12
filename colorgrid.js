05be87d312848493b8afc9475019602cbc0abbdb
Matthew Panebianco <mpane1@u.brockport.edu>
2015-07-12 16:53:55 -0400

    Commit Two
    Got the javascript to update but having issues with changing the size
    of the divs to properly fit within the containing div

--- colorgrid.js
+++ colorgrid.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-	var gridSize = 16; // default grid size
+	var ready_gridSize = 16; // default grid size
 	var whileCounter = 0; // initialized counter
-	while(whileCounter < gridSize){
-		for(var i = 0; i < gridSize; i++){
+	while(whileCounter < ready_gridSize){
+		for(var i = 0; i < ready_gridSize; i++){
 			$('div#container').append('<div class="pixel"></div>');
 		}
 		whileCounter++;
@@ -16,4 +16,32 @@ $('#container').on('mouseover', '*', function(){
 
 function resetPixels(){
 	$('#container div').removeClass('painted');
+	var input_gridSize = parseInt(prompt("Enter the number of pixels you'd like to see", 16));
+	console.log(input_gridSize); 
+	resizePixels(input_gridSize);
+}
+
+function resizePixels(resize_gridSize){
+	var containerWidth = $('div#container').css('width');
+	containerWidth = containerWidth.replace('px', '');
+	console.log(containerWidth);
+	var new_pixelSize = (containerWidth / resize_gridSize);
+	console.log(new_pixelSize);
+	var yourmom = $('.pixel').css('width');
+	console.log(yourmom); 
+	$('.pixel').width(new_pixelSize);
+	$('.pixel').height(new_pixelSize);
+	console.log(yourmom);
+	resetGrid(resize_gridSize);
+}
+
+function resetGrid(reset_gridSize){
+	$('div#container').html('');
+	var whileCounter = 0; // initialized counter
+	while(whileCounter < reset_gridSize){
+		for(var i = 0; i < reset_gridSize; i++){
+			$('div#container').append('<div class="pixel"></div>');
+		}
+		whileCounter++;
+	}
 }
